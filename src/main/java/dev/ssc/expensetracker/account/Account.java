6d412f43a862432c10f5d56a1e71bc0fae232142
shivendra-s-chouhan <shivendrasc2095@gmail.com>
2026-04-04 10:17:15 +0530

    initial commit expense tracker

---
+++ src/main/java/dev/ssc/expensetracker/account/Account.java
@@ -0,0 +1,46 @@
+package dev.ssc.expensetracker.account;
+
+public class Account {
+    private Long id;
+    private String name;
+    private double balance;
+
+    public Account(Long id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ src/main/java/dev/ssc/expensetracker/transaction/Transaction.java
@@ -0,0 +1,9 @@
+package dev.ssc.expensetracker.transaction;
+
+import java.time.LocalDate;
+
+public abstract class Transaction {
+    protected double amount;
+    protected LocalDate date;
+
+}
