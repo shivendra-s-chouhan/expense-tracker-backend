bb5aa6f648c2c739eba61d1f3b809f8b75daf384
shivendra-s-chouhan <shivendrasc2095@gmail.com>
2026-06-24 10:16:59 +0530

    send profile data based on token from frontend

--- src/main/java/dev/ssc/expensetracker/user/UserController.java
+++ src/main/java/dev/ssc/expensetracker/user/UserController.java
@@ -3,9 +3,12 @@ package dev.ssc.expensetracker.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @RestController
@@ -40,6 +43,49 @@ public class UserController {
   @DeleteMapping ("/{id}")
   public void deleteById(@PathVariable Integer id) {
     userRepository.deleteById(id);
+  }
+  
+  @GetMapping("/me")
+  public ResponseEntity<?> getCurrentUserProfile(){
+    String currentEmail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if("".equals(currentEmail)){
+      //get default user (userId = 1)
+      Optional<User> defaultUser = userRepository.findByEmail("john.doe@example.com");
+      if(defaultUser.isEmpty()){
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      }
+      
+      User updatedUser = new User (
+          defaultUser.get().id(),
+          defaultUser.get().name(),
+          defaultUser.get().userName(),
+          defaultUser.get().email(),
+          "", // wipe pwd before sending data to frontend
+          defaultUser.get().address(),
+          defaultUser.get().phone(),
+          defaultUser.get().website(),
+          defaultUser.get().company()
+      );
+      return ResponseEntity.ok(updatedUser);
+    }
+    return  userRepository.findByEmail(currentEmail)
+                .map(user -> {
+                  User updatedUser = new User (
+                      user.id(),
+                      user.name(),
+                      user.userName(),
+                      user.email(),
+                      "", // wipe pwd before sending data to frontend
+                      user.address(),
+                      user.phone(),
+                      user.website(),
+                      user.company()
+                  );
+                  return ResponseEntity.ok(updatedUser);
+                })
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    
+    
   }
   
   
